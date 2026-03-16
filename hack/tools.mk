c07463fe2ae4ec9907f95437de5d89ab3518f050
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2026-03-16 15:42:36 +0100

    Update dependency fluxcd/flux2 to v2.8.3 (#197)

--- hack/tools.mk
+++ hack/tools.mk
@@ -5,7 +5,7 @@
 GLK_PRETTIFY        := $(TOOLS_BIN_DIR)/prettify
 
 # renovate: datasource=github-releases depName=fluxcd/flux2
-FLUX_CLI_VERSION ?= v2.8.2
+FLUX_CLI_VERSION ?= v2.8.3
 GLK_PRETTIFY_VERSION = $(shell git rev-parse HEAD)
 
 FLUX_CLI ?= $(TOOLS_DIR)/bin/$(SYSTEM_NAME)-$(SYSTEM_ARCH)/flux
